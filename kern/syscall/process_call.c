32823ca93fe551180458d888e4ebc52e42da8e09
Abhijeet Mishra <amishr3@zeus-2.vse.gmu.edu>
2018-10-14 14:06:11 -0400

    Minor changer #1. Not tested till now.

---
+++ kern/syscall/process_call.c
@@ -0,0 +1,101 @@
+
+//Waitpid
+#include <types.h>
+#include <kern/errno.h>
+#include <kern/fcntl.h>
+#include <kern/unistd.h>
+#include <lib.h>
+#include <proc.h>
+#include <current.h>
+#include <addrspace.h>
+#include <vm.h>
+#include <vfs.h>
+#include <openfile.h>
+#include <filetable.h>
+#include <syscall.h>
+#include <test.h>
+#include <synch.h>
+
+
+pid_t sys_waitpid(pid_t pid, int *status, int options)
+{
+    struct thread* curt = curthread;
+	struct proc* curp = curt->t_proc;
+	struct proc* childp;
+    bool procfound = false;
+    struct proc *tempHolder;
+    tempHolder = curp->child_list;
+
+    if(options != 0)
+	{
+		return EINVAL;
+	}
+
+    lock_acquire(curp->child_lock);
+    for(int i=0;i<childlimit;i++)
+    {
+      
+        if ((tempHolder+i)->pid == pid)
+        {
+            childp = (tempHolder+i);
+            procfound = true;
+            break;
+        }
+    }
+    if(!procfound)
+    {
+        return ECHILD;
+    }
+
+    lock_release(curp->child_lock);
+    if(childp->status == zombie)
+    {
+        lock_acquire(curp->child_lock);
+        status = &childp->returnValue;
+        //proc_destroy(childp); //cant be done here. To discuss this tomorrows
+        lock_release(curp->child_lock);
+
+        return 0;
+    }
+
+    P(childp->p_sem);
+
+    lock_acquire(curp->child_lock);
+    status = &childp->returnValue;
+    childp->status = zombie;
+    //proc_destroy(childp);
+    lock_release(curp->child_lock);
+    return 0;
+
+}
+
+
+void sys_exit(int exitcode)
+{
+
+    struct thread* curt = curthread;
+	struct proc* curp = curt->t_proc;
+	struct proc* childp;
+    struct proc *tempHolder;
+    tempHolder = curp->child_list;
+    lock_acquire(curp->child_lock);
+    for(int i=0;i<childlimit;i++)
+    {
+      
+        if ((tempHolder+i) != NULL )
+        {
+            proc_destroy((tempHolder+i));
+        }
+        else if ((tempHolder+i)->status == zombie)
+        {
+            proc_destroy(tempHolder+i);
+        }
+    }
+    lock_release(curp->child_lock);
+
+
+    curp->returnValue = exitcode;
+    V(childp->p_sem);
+
+
+}
